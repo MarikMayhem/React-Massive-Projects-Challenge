c2fae16be78b064c5b0395e9eeee5d3ce4eea02b
Yuliyan Ivanov <winsomewe.kek37@gmail.com>
2020-12-15 13:43:37 +0200

    Fix the issues with the modal

--- 6.Movie DB/movie-db/src/Components/MoviePresentation/MoviePresentation.js
+++ 6.Movie DB/movie-db/src/Components/MoviePresentation/MoviePresentation.js
@@ -1,5 +1,5 @@
 
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './MoviePresentation.scss'
 import { FaPlayCircle } from 'react-icons/fa';
 import { AiOutlineClose } from 'react-icons/ai';
@@ -9,22 +9,34 @@ import Modal from '../UI/Modal/Modal';
 const MoviePresentation = ({ id, backdrop, title }) => {
     const [movieTrailerLink, setMovieTrailerLink] = useState('');
     const [displayModal, setDisplayModal] = useState(false);
-    const trailerIframe = <iframe title="trailer" className="trailer" src={`https://www.youtube.com/embed/${movieTrailerLink}`} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    const [trailerIframe, setTrailerIframe] = useState('');
+    const iframeRef = useRef(null);
+    const MODAL_OPEN_CLASS = "open-modal";
 
     useEffect(() => {
         axios.get(`movie/${id}/videos?&language=en-US`)
             .then(res => setMovieTrailerLink(res.data.results[0].key))
-    }, [id])
+        setTrailerIframe(<iframe ref={iframeRef} title="trailer" className="trailer" src={`https://www.youtube.com/embed/${movieTrailerLink}?rel=0&controls=0&hd=1&showinfo=0&enablejsapi=1`} frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>)
+    }, [id, movieTrailerLink])
+
+    const openModalHandler = () => {
+        setDisplayModal(true);
+        document.body.classList.add(MODAL_OPEN_CLASS);
+    }
 
     const closeModalHandler = () => {
         setDisplayModal(false);
+        document.body.classList.remove(MODAL_OPEN_CLASS);
+
+        //Communicate with youtubejs api to stop playback
+        iframeRef.current.contentWindow.postMessage('{"event":"command","func":"stopVideo","args":""}', '*')
     };
     return (
         <section className="movie-presentation">
             <img className="movie-backdrop"
                 src={`https://image.tmdb.org/t/p/original/${backdrop}`}
                 alt="background" />
-            <FaPlayCircle onClick={() => setDisplayModal(!displayModal)} className="play-icon" />
+            <FaPlayCircle onClick={() => setDisplayModal(openModalHandler)} className="play-icon" />
 
             <Modal show={displayModal} modalClosed={closeModalHandler}>
                 <div className="title">
@@ -32,7 +44,6 @@ const MoviePresentation = ({ id, backdrop, title }) => {
                     <AiOutlineClose className="close" onClick={closeModalHandler} />
                 </div>
                 {trailerIframe}
-
             </Modal>
         </section>
     );
